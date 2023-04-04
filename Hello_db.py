30058b665881c9ac0ce87dba52ec06359347d3bb
GofinAI <128676791+GofinAI@users.noreply.github.com>
2023-04-04 12:30:40 +0530

    Update Hello_db.py

--- Hello_db.py
+++ Hello_db.py
@@ -1,27 +1,24 @@
+from flask import Flask, render_template
 import mysql.connector as db
 
-# Establish a connection to the database
-cnx = db.connect(
+app = Flask(__name__)
+
+mydb = db.connect(
     host = "gofin-aurora-instance-1.ci0rkg2zgzsd.us-east-1.rds.amazonaws.com",
     user = "malikam",
     password = "Malika@98966",
     database = "usda"
 )
 
-# Create a cursor object to execute queries
-cursor = cnx.cursor()
+@app.route('/')
+def index():
+    # Execute a SELECT query
+    cursor = mydb.cursor()
+    cursor.execute("SELECT * FROM S_GROUP_DESC")
+    results = cursor.fetchall()
+    
+    # Render the webpage with the query results
+    return render_template('index.html', results=results)
 
-# Execute a query
-query = "SELECT * FROM S_GROUP_DESC"
-cursor.execute(query)
-
-# Fetch the result
-result = cursor.fetchall()
-
-# Print the result
-for row in result:
-    print(row)
-
-# Close the cursor and the connection
-cursor.close()
-cnx.close()
+if __name__ == '__main__':
+    app.run()
